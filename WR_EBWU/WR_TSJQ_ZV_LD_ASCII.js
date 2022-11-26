2567edab4e0be815d2ddbe2140d78727bdd0f575
wrvr <974054997@qq.com>
2022-11-27 00:50:42 +0800

    Sdvu lw sjbx; Ukyp ascii ld ztfr tsjq--wrvr.

---
+++ WR_EBWU/WR_TSJQ_ZV_LD_ASCII.js
@@ -0,0 +1,59 @@
+
+const ascii_ld_ztfr_zv_zqjp_ymrg = require("../AFOA_BX/ascii_ld_ztfr_zv_zqjp_ymrg");
+const fs=require('fs')
+const encoding=require('encoding')
+function WR_TSJQ_ZV_LD_ASCII(IOWR_AFOA) {
+    var UXUX_YHLD = typeof (IOWR_AFOA)
+    if (UXUX_YHLD != "object") {
+        throw new Error("MCVN UXUX MSOX , AOAO JI object:" + UXUX_YHLD)
+    }
+    var BQEO_1 = IOWR_AFOA.WR_AFOA_BQEO.replace(/^\s+|\s+$/g, "")
+    BQEO_1 = BQEO_1.replace(/(\n|^)[^\S\n]+/g, "$1").replace(/[^\S\n]+(\n|$)/g, "$1")
+    var VNWM_MCVN_1 = IOWR_AFOA.WR_AFOA_MCVN.match(/[^,]+=[^,]+/g);
+    var LG
+    var DS
+    var vkvy_1
+    var gkqj_rjqt_ahtz=false;
+    if (VNWM_MCVN_1 == null) {
+        
+    } else {
+        VNWM_MCVN_1.forEach(RNSF => {
+            switch (true) {
+                case /LG\b/i.test(RNSF):
+                    IOWR_LG_XBST = RNSF.match(/(.*)\s*=\s*(.*)/)
+                    if (IOWR_LG_XBST == null) {
+                        throw new Error("[MCVN FTPJ]" + RNSF + "<--" + IOWR_AFOA.WR_AFOA_MCVN)
+                    } else {
+                        LG = IOWR_LG_XBST[2]
+                    }
+                    break;
+                case /DS\b/i.test(RNSF):
+                    IOWR_DS_XBST = RNSF.match(/(.*)\s*=\s*(.*)/)
+                    if (IOWR_DS_XBST == null) {
+                        throw new Error("[MCVN FTPJ]" + RNSF + "<--" + IOWR_AFOA.WR_AFOA_MCVN)
+                    } else {
+                        DS = IOWR_DS_XBST[2]
+                    }
+                    break;
+                case /\brjqt\b/i.test(RNSF):
+                    gkqj_rjqt_ahtz=true;    
+                    vkvy_1=RNSF.match(/(?<==)\w+/)
+                    if(vkvy_1==null){
+                        throw new Error('csrf-vkvy mcvn nrap-'+RNSF)
+                    }
+                    if(!fs.existsSync(BQEO_1)){
+                        throw new Error('csrf-yxna ac zznq-'+BQEO_1)
+                    }
+                break;
+                default:
+                    throw new Error("[ACUN MCVN WU]" + RNSF + "<--" + AFOA_UJ.WR_AFOA_MCVN)
+            }
+        });
+    }
+    if(gkqj_rjqt_ahtz){
+        BQEO_1=encoding.convert(fs.readFileSync(BQEO_1),'utf8',vkvy_1).toString().replace(/\r/g,"")
+    }
+    var RJSE_1 = ascii_ld_ztfr_zv_zqjp_ymrg(BQEO_1)
+    return RJSE_1
+}
+module.exports = WR_TSJQ_ZV_LD_ASCII;

ecbeb41ea63c1c9dd76ed9a3d0c45cae91718493
wrvr <974054997@qq.com>
2022-12-24 01:40:50 +0800

    deleted:    "AFOA_BX/\357\277\275\304\274\357\277\275\327\252\357\277\275\326\267\357\277\275\357\277\275\357\277\275.js" 	deleted:    "AFOA_BX/\357\277\275\326\267\357\277\275\357\277\275\357\277\275\327\252\357\277\275\304\274\357\277\275.js"
    	modified:   GLRH.js

--- GLRH.js
+++ GLRH.js
@@ -1,7 +1,25 @@
-const fs=require('fs')
-const ce_ebwu_yhrj_sjbx_diyc_eysj_zjlz = require('./AFOA_BX/ce_ebwu_yhrj_sjbx_diyc_eysj_zjlz')
-var yxna_1="./AFOA_BX/KPLU/ce_ebwu_kplu_zv_yhrj_sjbx.java"
-var yxna_2="./AFOA_BX/KPLU/SI_ZTWM_3.java"
-var yxna_3="D:/RSGM/nodejs/UUVO_OUSS/AFOA_BX/KPLU/词典360万单词量.txt"
-var uu=String.fromCodePoint(0x78 ,0X30 ,0X38)
-throw uu
+// const fs=require('fs')
+// const ce_ebwu_yhrj_sjbx_diyc_eysj_zjlz = require('./AFOA_BX/ce_ebwu_yhrj_sjbx_diyc_eysj_zjlz')
+// var yxna_1="./AFOA_BX/KPLU/ce_ebwu_kplu_zv_yhrj_sjbx.java"
+// var yxna_2="./AFOA_BX/KPLU/SI_ZTWM_3.java"
+// var yxna_3="D:/RSGM/nodejs/UUVO_OUSS/AFOA_BX/KPLU/词典360万单词量.txt"
+// var uu=String.fromCodePoint(0x78 ,0X30 ,0X38)
+// throw uu
+// const child_process = require('child_process');
+const { spawn } = require('child_process');
+
+const batPath = "D:/RSGM/nodejs/启动工作助手2.bat";
+
+const bat = spawn('cmd.exe', ['/c', batPath]);
+
+bat.stdout.on('data', data => {
+  console.log(data.toString());
+});
+
+bat.stderr.on('data', data => {
+  console.error(data.toString());
+});
+
+bat.on('exit', code => {
+  console.log(`Child exited with code ${code}`);
+});

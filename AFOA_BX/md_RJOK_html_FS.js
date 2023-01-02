9f83bcba10ead1de1147233a49a607dd9ab1c677
wrvr <974054997@qq.com>
2023-01-02 18:11:54 +0800

    Qity md-md-md ld html dk atvn.

--- AFOA_BX/md_RJOK_html_FS.js
+++ AFOA_BX/md_RJOK_html_FS.js
@@ -1,9 +1,11 @@
 const marked = require('marked');
 const wk_mk_html_code = require('./wk_mk_html_code');
-function  md_RJOK_html_fs(rj_kp){
-    var rj_1=rj_kp
+function md_RJOK_html_fs(rj_kp, diwr_mcvn) {
+    var rj_1 = rj_kp
     rj_1 = marked.marked(rj_1)
-    rj_1=wk_mk_html_code(rj_1,'javascript')
+    if (diwr_mcvn != null && diwr_mcvn.gkqj_dyvy_nppr) {
+        rj_1 = wk_mk_html_code(rj_1, 'javascript')
+    }
     return rj_1
 }
-module.exports=md_RJOK_html_fs
+module.exports = md_RJOK_html_fs
